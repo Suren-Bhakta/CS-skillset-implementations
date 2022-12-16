7750d7a3be1106a86a087a113b77ebb3429b8825
Suren-Bhakta <113945491+Suren-Bhakta@users.noreply.github.com>
2022-12-16 01:05:18 -0600

    Update search.py

--- search.py
+++ search.py
@@ -1,21 +1,7 @@
-# File: Work.py
-#  Description: prints the minimum lines of code that can be written before Chris passes out
-#  Student Name: Suren Bhakta
-#  Student UT EID: ssb2943
-#  Partner Name: Karim Ladak
-#  Partner UT EID: kal3635
-#  Course Name: CS 313E
-#  Unique Number: 52520
-#  Date Created: 9/24/2022
-#  Date Last Modified: 9/29/2022
-
 import sys
 import time
 
-# Input: v an integer representing the minimum lines of code and
-#        k an integer representing the productivity factor
-# Output: computes the sum of the series (v + v // k + v // k**2 + ...)
-#         returns the sum of the series
+
 def sum_series (v, k):
   # continues to divide the numbers and add to the sum until the sum lines left is zero
   sum = 0
@@ -29,9 +15,7 @@ def sum_series (v, k):
   return sum
 
 
-# Input: n an integer representing the total number of lines of code
-#        k an integer representing the productivity factor
-# Output: returns v the minimum lines of code to write using linear search
+
 def linear_search (n, k):
   # Goes through each line needed to write and deduces down to the minimum to be written
   for i in range(n+1):
@@ -43,9 +27,7 @@ def linear_search (n, k):
 
 
 
-# Input: n an integer representing the total number of lines of code
-#        k an integer representing the productivity factor
-# Output: returns v the minimum lines of code to write using binary search
+
 def binary_search (n, k):
   low = 0
   mid=0
